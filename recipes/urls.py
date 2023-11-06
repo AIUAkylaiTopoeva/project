a7f333e045114672d1f1a2e29f5b0143d3554893
AkylaiTopoeva <akylaytop@gmail.com>
2023-11-06 23:27:34 +0600

    add func for category

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.all_posts, name="posts"),
-    path('post/<str:id>/', views.post_by_id, name="post_by_id"),
-    path('create/post/', views.create_post, name="create_post"),
-    path('update/post/<str:id>/', views.update_post, name="update_post"),
-    path('delete/post/<str:id>/', views.delete_post, name='delete_post'),
+    path('',views.all_recipes, name="recipe"),
+    path('recipe/<str:id>/', views.post_by_id, name="recipe_by_id"),
+    path('create/recipe/', views.create_recipe, name="create_recipe"),
+    path('update/recipe/<str:id>/', views.update_recipe, name="update_recipe"),
+    path('delete/recipe/<str:id>/', views.delete_recipe, name='delete_recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -6,12 +6,13 @@ import string
 from .models import Recipe, Category
 from .forms import CategoryForm, RecipeForm
 
-def all_posts(request:HttpRequest):
+def all_recipes(request:HttpRequest):
     context = {
-        'posts':Recipe.objects.all()
+        'recipe':Recipe.objects.all()
     }
 
-    return render(request,'index.html',context=context)
+    return render(request,'recipes/index.html',context=context)
+
 
 def post_by_id(request, id):
     
@@ -23,7 +24,7 @@ def post_by_id(request, id):
 
 
 @csrf_exempt
-def create_post(request:HttpRequest):
+def create_recipe(request:HttpRequest):
     form = RecipeForm()
     if(request.method == 'POST'):
         form = RecipeForm(data=request.POST)
@@ -34,7 +35,18 @@ def create_post(request:HttpRequest):
     return render(request, "post-form.html", context)
 
 @csrf_exempt
-def update_post(request:HttpRequest, id:string):
+def create_category(request:HttpRequest):
+    form = CategoryForm()
+    if(request.method == 'POST'):
+        form = CategoryForm(data=request.POST)
+        form.save()
+        return redirect('recipe')
+
+    context = {'form':form,}
+    return render(request, "post-form.html", context)
+
+@csrf_exempt
+def update_recipe(request:HttpRequest, id:string):
     recipe = Recipe.objects.get(id=id)
     print(recipe)
     form = RecipeForm(instance=recipe)
@@ -47,30 +59,10 @@ def update_post(request:HttpRequest, id:string):
     return render(request, "posts-form.html", context)
 
 @csrf_exempt
-def delete_post(request, id):
+def delete_recipe(request, id):
     recipe = Recipe.objects.get(id=id)
     if(request.method == 'POST'):
         recipe.delete()
         return redirect('recipe')
     context = {'recipe':recipe}
     return render(request, 'delete-post.html')
-
-@csrf_exempt
-def create_category(request:HttpRequest):
-    form = CategoryForm()
-    if(request.method == 'POST'):
-        form = CategoryForm(data=request.POST)
-        form.save()
-        return redirect('category')
-
-    context = {'form':form,}
-    return render(request, "post-category.html", context)
-
-@csrf_exempt
-def delete_category(request, id):
-    category = Category.objects.get(id=id)
-    if request.method == 'POST':
-        category.delete()
-        return redirect('category')
-    context = {'category':category}
-    return render(request, 'category-delete.html')
